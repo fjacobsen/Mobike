71cf8a5149486fcabc7dde8a79a8099ad510bafe
FILIPE MARTINS JACOBSEN <11205108@l1726934.portoalegre.pucrsnet.br>
2015-11-03 20:27:39 -0200

    #1

---
+++ bike.cc
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+class mobike {
+
+private:
+    float credito;
+public:
+
+    mobike();
+
+    void retirar(int r);
+    void devolver(int d);
+    void verificacredito (int v);
+};
+
+int main ()
+{
+    return 0;
+}
+
+
+
+
