a4d16d075d107c555258cf707ab57055bab978bc
FELIPE VARGAS DE WILBERT <11204875@l1818129.portoalegre.pucrsnet.br>
2015-11-10 20:27:23 -0200

    CadastroClientes

---
+++ CadBike.cc
@@ -0,0 +1,41 @@
+#include<fstream>
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+
+// Cria input file stream (ifstream)
+    ifstream arq;
+
+    cout << "Abrindo arquivo texto..." << endl;
+
+    // Abre arquivo
+    arq.open( "CadastroBike.txt" , ios::in );
+
+    // Se houver erro, sai do programa
+    if (!arq.is_open())
+        return 0;
+    // Agora, lê n registros numéricos
+    do
+    {
+        int numeroBike;
+                arq >> numeroBike ;
+        if(!arq.fail()) {
+            cout << "Numero da Bike:" << numeroBike << endl;
+        }
+    } while(arq.good());
+    if(arq.bad() || !arq.eof()) {
+        cout << "Erro fatal!" << endl;
+    }
+    cout << "Fechando o arquivo..." << endl;
+    arq.close();
+
+    return 0;
+}
+
+
+
+
+
