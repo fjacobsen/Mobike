822d2eaecb45feea6d435c60cb34a5d1f5b8c6db
RSFAY <rodrigosorianofay@gmail.com>
2015-11-18 23:11:23 -0200

    #2

--- bike.h
+++ bike.h
@@ -1,18 +1,14 @@
-
 #include <iostream>
-
 using namespace std;
-
 
 class mobike {
 
 private:
     float credito;
  vector<int> bikes;
+
 public:
-
-   // mobike();
-
+    mobike();
     void retirar(int r);
     void devolver(int d);
     void verificacredito (int v);
